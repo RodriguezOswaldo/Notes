c988abc1ef2066ad2b40c0c797a9c2ee2b4c10e2
OwnOS <Owner2303@gmail.com>
2021-04-03 10:38:32 -0400

    add/delete for all the options

--- script.js
+++ script.js
@@ -8,16 +8,26 @@ function addThoughtsItem(){
     li.appendChild(document.createTextNode(thoughts.value));
     ul.appendChild(li);
 }
-function addThoughtsItem(){
-    let ul = document.getElementById('thoughts-list');
-    let thoughts = document.getElementById('thoughts');
+function addBlessingsItem(){
+    let ul = document.getElementById('blessings-list');
+    let blessings = document.getElementById('blessings');
     let li = document.createElement('li');
-    li.setAttribute('id', thoughts.value);
-    li.appendChild(document.createTextNode(thoughts.value));
+    li.setAttribute('id', blessings.value);
+    li.appendChild(document.createTextNode(blessings.value));
+    ul.appendChild(li);
+}
+function addWarningsItem(){
+    let ul = document.getElementById('warnings-list');
+    let warnings = document.getElementById('warnings');
+    let li = document.createElement('li');
+    li.setAttribute('id', warnings.value);
+    li.appendChild(document.createTextNode(warnings.value));
     ul.appendChild(li);
 }
 
+warnings
 //delete functions 
+
 function deleteThoughtsItem(){
     let ul = document.getElementById('thoughts-list');
     let thoughts = document.getElementById('thoughts');
@@ -25,6 +35,19 @@ function deleteThoughtsItem(){
     ul.removeChild(item);
 }
 
+function deleteBlessingsItem(){
+    let ul = document.getElementById('blessings-list');
+    let blessings = document.getElementById('blessings');
+    let item = document.getElementById(blessings.value);
+    ul.removeChild(item);
+}
+
+function deleteWarningsItem(){
+    let ul = document.getElementById('warnings-list');
+    let blessings = document.getElementById('warnings');
+    let item = document.getElementById(warnings.value);
+    ul.removeChild(item);
+}
 function callAddItem(){
     addThoughtsItem();
 }
